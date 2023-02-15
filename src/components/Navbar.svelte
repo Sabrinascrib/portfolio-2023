197e2069cdf4fa6607f049a035410bb8924d25fa
sabrina <sabrinaabrodi20@gmail.com>
2023-02-15 21:30:06 +0100

    feat: navbar structure

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { append } from "svelte/internal";
+</script>
+
+<nav class="flex flex-row justify-between p-9">
+  <a href="/" class="bg-yellow-300 text-xl font-extrabold uppercase italic px-2"
+    >Sabrina Abrodi</a
+  >
+
+  <div class="">
+    <a href="#">Slay</a>
+    <a href="#">Yayy</a>
+    <a href="#">Slay</a>
+    <a href="#">Yayy</a>
+  </div>
+</nav>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap");
+
+  a {
+    font-family: "Fjalla One", sans-serif;
+  }
+</style>
