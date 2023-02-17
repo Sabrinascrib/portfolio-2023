fab17cf38ec65a1a1024f9ca19f94adbda61cd5b
sabrina <sabrinaabrodi20@gmail.com>
2023-02-17 17:19:24 +0100

    add: font

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,24 +1,27 @@
 <script>
+  import sabrinaabroditext from "../images/sabrinaabroditext.png";
   import { append } from "svelte/internal";
 </script>
 
-<nav class="flex flex-row justify-between p-9">
-  <a href="/" class="bg-yellow-300 text-xl font-extrabold uppercase italic px-2"
-    >Sabrina Abrodi</a
-  >
+<div class="flex flex-col  place-items-center pt-10">
+  <img src={sabrinaabroditext} class="w-96" alt="my name" />
 
-  <div class="">
-    <a href="#">Home</a>
-    <a href="#">About</a>
-    <a href="#">Projects</a>
-    <a href="#">Contact</a>
-  </div>
-</nav>
+  <nav class="flex flex-row justify-between mt-10 px-20  w-[1000px]">
+    <div class="">
+      <a href="#">Home</a>
+      <a href="#" class="ml-4">About</a>
+    </div>
+
+    <div class="">
+      <a href="#" class="mr-4">Projects</a>
+      <a href="#">Contact</a>
+    </div>
+  </nav>
+</div>
 
 <style>
-  @import url("https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap");
-
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap");
   a {
-    font-family: "Fjalla One", sans-serif;
+    font-family: "Poppins", sans-serif;
   }
 </style>
