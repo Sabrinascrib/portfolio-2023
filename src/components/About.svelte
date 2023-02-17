a2abe80db80b4acdee806029d39fab9f30d24e38
sabrina <sabrinaabrodi20@gmail.com>
2023-02-17 17:19:44 +0100

    feat: about component

---
+++ src/components/About.svelte
@@ -0,0 +1,17 @@
+<script>
+  import aboutmetext from "../images/aboutmetext.png";
+</script>
+
+<div class="max-w-[1000px] mx-auto">
+  <img src={aboutmetext} class="w-56 mb-4" alt="" />
+  <p>
+    I‘m a 16 year old self-taught Front-End Developer based in Germany. My
+    interest in web development started back in 2017, I tried <br /> to develop
+    a website out of curiosity after seeing my brother doing it. Ever since,
+    I’ve been loving it. <br /> <br /> When I’m not doing anything coding related,
+    I’m a high schooler who enjoys graphic designing, video editing and working out.
+  </p>
+</div>
+
+<style>
+</style>
