1b7102e0e1f7734961bf2541bd880589ec37183c
sabrina <sabrinaabrodi20@gmail.com>
2023-02-18 18:29:50 +0100

    feat: contact component structure

---
+++ src/components/Contact.svelte
@@ -0,0 +1,12 @@
+<script>
+  import contactmetext from "../images/contactmetext.png";
+</script>
+
+<div class="max-w-[1000px] mx-auto">
+  <img src={contactmetext} class="w-56 mb-4" alt="" />
+  <p>
+</p>
+</div>
+
+<style>
+</style>
