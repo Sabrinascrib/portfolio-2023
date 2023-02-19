1bbf862b6683770a58333561b580952898168a41
sabrina <sabrinaabrodi20@gmail.com>
2023-02-19 18:28:25 +0100

    feat: importing footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script>
+  import "../app.css";
   import About from "../components/About.svelte";
   import Contact from "../components/Contact.svelte";
+  import Footer from "../components/Footer.svelte";
   import Navbar from "../components/Navbar.svelte";
 </script>
 
-<Navbar />
-<About />
-<Contact />
+<main class="w-full h-full">
+  <Navbar />
+  <About />
+  <Contact />
+  <Footer />
+</main>
+
+<style>
+</style>
