4c111690dafdbc1e953f5ea33cd6e61bdd6de52c
sabrina <sabrinaabrodi20@gmail.com>
2023-02-19 18:24:59 +0100

    feat: projects

---
+++ src/components/Projects.svelte
@@ -0,0 +1,11 @@
+<script>
+  import myprojectstext from "../images/myprojectstext.png";
+</script>
+
+<div class="max-w-[1000px] mx-auto">
+  <img src={myprojectstext} class="w-56 mb-4" alt="" />
+  <p />
+</div>
+
+<style>
+</style>
