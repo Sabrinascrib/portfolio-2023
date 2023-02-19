8bbfa7e0daff420f1f244f98361321cb296a57c4
sabrina <sabrinaabrodi20@gmail.com>
2023-02-19 18:23:54 +0100

    feat: footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+  import sabrinaabroditext from "../images/sabrinaabroditext.png";
+</script>
+
+<div class="flex flex-col  place-items-center pt-10">
+  <img src={sabrinaabroditext} class="w-96" alt="my name" />
+
+  <div class="links">
+    <a href="# " />
+  </div>
+</div>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap");
+  a {
+    font-family: "Poppins", sans-serif;
+  }
+</style>
