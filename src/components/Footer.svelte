4b023fee9cb5c2df15ac0faa8e0777821ac88822
sabrina <sabrinaabrodi20@gmail.com>
2023-02-21 19:55:12 +0100

    feat: adding footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,23 @@
 <script>
   import sabrinaabroditext from "../images/sabrinaabroditext.png";
+
+  import instagramtext from "../images/instagramtext.png";
+  import linkedintext from "../images/linkedintext.png";
+  import githubtext from "../images/githubtext.png";
 </script>
 
 <div class="flex flex-col  place-items-center pt-10">
-  <img src={sabrinaabroditext} class="w-96" alt="my name" />
-
-  <div class="links">
-    <a href="# " />
+  <h3>Designed and build by Sabrina Abrodi</h3>
+  <div class="flex flex-row justify-between w-96">
+    <a href="" class="duration-100 hover:mt-[-3px] "
+      ><img class="h-8" src={instagramtext} alt="" /></a
+    >
+    <a href="" class="duration-100 hover:mt-[-3px] "
+      ><img class="h-8" src={linkedintext} alt="" /></a
+    >
+    <a href="" class="duration-100 hover:mt-[-3px] "
+      ><img class="h-8" src={githubtext} alt="" /></a
+    >
   </div>
 </div>
 
