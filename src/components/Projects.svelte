c7ec0d0ce41046e7ffc7860e7eeec4d119a350ba
sabrina <sabrinaabrodi20@gmail.com>
2023-02-21 19:55:33 +0100

    fix: projects layout

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,10 +1,25 @@
 <script>
+  import kicksfixtext from "../images/kicksfixtext.png";
+
   import myprojectstext from "../images/myprojectstext.png";
 </script>
 
-<div class="max-w-[1000px] mx-auto">
-  <img src={myprojectstext} class="w-56 mb-4" alt="" />
-  <p />
+<div class="max-w-[1000px] mx-auto ">
+  <div class="flex flex-row flex-wrap w-96">
+    <img src={myprojectstext} class="h-14 mb-4" alt="" />
+    <div class="project p-14 shadow-lg rounded-lg">
+      <img src={kicksfixtext} alt="" class="w-16" />
+    </div>
+    <div class="project p-14 shadow-lg rounded-lg">
+      <img src={animeflixtext} alt="" class="w-16" />
+    </div>
+    <div class="project p-14 shadow-lg rounded-lg">
+      <img src={kicksfixtext} alt="" class="w-16" />
+    </div>
+    <div class="project p-14 shadow-lg rounded-lg">
+      <img src={kicksfixtext} alt="" class="w-16" />
+    </div>
+  </div>
 </div>
 
 <style>
