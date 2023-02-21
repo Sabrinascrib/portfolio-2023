85392d6aaf4938c5ed7519b3ce1c4a75bf713a35
sabrina <sabrinaabrodi20@gmail.com>
2023-02-21 19:54:55 +0100

    feat: contect text

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <div class="max-w-[1000px] mx-auto">
-  <img src={contactmetext} class="w-56 mb-4" alt="" />
+  <img src={contactmetext} class="h-14 mb-4" alt="" />
   <p>
-</p>
+    The fastest way to get in touch with me is to send me an email, whether you
+    have a question or just want to say hi, I'll try my best to get back to you.
+  </p>
 </div>
 
 <style>
